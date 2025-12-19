bbac15ce769e70b7f91035b5662fa8fabfd8dd8d
Abo-Omar-74 <mohamedaboomar1211@gmail.com>
2025-12-20 00:04:28 +0200

    refactor: select more attribute and fix alise

--- internal/database/list_products_template.sql
+++ internal/database/list_products_template.sql
@@ -4,6 +4,10 @@ SELECT
   p.name,
   p.slug,
   p.brand,
+  p.description,
+  p.category_id,
+  p.stock_quantity AS total_stock,
+  pv.stock_quantity AS item_stock,
   pv.price,
   pv.primary_image_url,
   p.in_stock
@@ -17,7 +21,7 @@ WHERE p.store_id = $1
   AND p.deleted_at IS NULL
   AND ($4::BIGINT IS NULL OR p.category_id = $4)
   AND ($5::TEXT IS NULL OR p.brand = $5)
-  AND ($6::DECIMAL IS NULL OR v.price >= $6)
-  AND ($7::DECIMAL IS NULL OR v.price <= $7)
+  AND ($6::DECIMAL IS NULL OR pv.price >= $6)
+  AND ($7::DECIMAL IS NULL OR pv.price <= $7)
 ORDER BY p.created_at DESC
 LIMIT $2 OFFSET $3;
