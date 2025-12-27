294a3ac1ca30706faa9d1bc6fa5fab398087c58b
Abo-Omar-74 <mohamedaboomar1211@gmail.com>
2025-12-27 21:54:36 +0200

    feat: add auth helper functions

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -1,5 +1,57 @@
 package utils
 
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Password hashing
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+}
+
+// JWT generation
+func GenerateJWT(userID int64, role string, storeID *int64, secret string, duration time.Duration) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+		"exp":     time.Now().Add(duration).Unix(),
+	}
+
+	if storeID != nil {
+		claims["store_id"] = *storeID
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
+
+// JWT parsing
+func ParseJWT(tokenStr, secret string) (*jwt.Token, jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, nil, errors.New("invalid token")
+	}
+
+	return token, claims, err
+}
+
 func InterfaceSlice[T any](s []T) []interface{} {
 	out := make([]interface{}, len(s))
 	for i := range s {
